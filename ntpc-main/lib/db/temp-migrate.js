aff3167799a0d7257cdd268a37c1170582deae8a
Biniam Haileyesus <32872040+Biniamh@users.noreply.github.com>
2026-05-19 06:23:12 -0300

    Delete ntpc-main directory

--- ntpc-main/lib/db/temp-migrate.js
+++
@@ -1,4 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const sql = "ALTER TABLE ey_events ADD COLUMN IF NOT EXISTS start_date TEXT NOT NULL DEFAULT ''; ALTER TABLE ey_events ADD COLUMN IF NOT EXISTS end_date TEXT NOT NULL DEFAULT ''; UPDATE ey_events SET start_date = date, end_date = date WHERE date IS NOT NULL; ALTER TABLE ey_events DROP COLUMN IF EXISTS date;";
-pool.query(sql).then(res => { console.log('Migration applied:', res.command); return pool.end(); }).catch(err => { console.error(err); pool.end(); process.exit(1); });
